6dc54733d5b2e27eb819ee771afa4b1984eaa425
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-07-22 14:02:06 -0300

    Incluindo testes em Categoria

--- src/main/java/com/generation/projetofarmacia/model/Categoria.java
+++ src/main/java/com/generation/projetofarmacia/model/Categoria.java
@@ -22,26 +22,17 @@ public class Categoria {
     @Size(min = 5, max = 255, message = "O atributo deve ter no mínimo 5 e no máximo 255 caracteres")
     private String descricao;
 
-
-
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
     @JsonIgnoreProperties("categoria")
     private List<Produto> produto;
 
-    public List<Produto> getProduto() {
-        return produto;
-    }
-
-    public void setProduto(List<Produto> produto) {
-        this.produto = produto;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    public Categoria(Long id, String nome, String descricao) {
+        this.id = id;
         this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Categoria() {
     }
 
     public long getId() {
@@ -52,11 +43,27 @@ public class Categoria {
         this.id = id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public List<Produto> getProduto() {
+        return produto;
+    }
+
+    public void setProduto(List<Produto> produto) {
+        this.produto = produto;
     }
 }
---
+++ src/test/java/com/generation/projetofarmacia/controller/CategoriaControllerTest.java
@@ -0,0 +1,84 @@
+package com.generation.projetofarmacia.controller;
+
+import com.generation.projetofarmacia.model.Categoria;
+import com.generation.projetofarmacia.repository.CategoriaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CategoriaControllerTest {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private CategoriaController categoriaController;
+
+    @Test
+    @DisplayName("✔ Cadastrar uma nova categoria")
+    public void deveCadastrarUmaNovaCategoria() {
+        HttpEntity<Categoria> corpoRequisicao = new HttpEntity<Categoria>(new Categoria(0L,
+                "Nova Categoria", "Descrição da nova categoria"));
+
+        ResponseEntity<Categoria> corpoResposta = testRestTemplate
+                .exchange("/categorias", HttpMethod.POST, corpoRequisicao, Categoria.class);
+
+        assertEquals(HttpStatus.CREATED, corpoResposta.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("✔ Não repetir uma categoria")
+    public void naoDeveRepetirCategoria() {
+        categoriaRepository.save(new Categoria(0L, "Nova Categoria", "Descrição da nova categoria"));
+
+        HttpEntity<Categoria> corpoRequisicao = new HttpEntity<Categoria>(new Categoria(0L, "Nova Categoria", "Descrição da nova categoria"));
+
+        ResponseEntity<String> corpoResposta = testRestTemplate
+                .exchange("/categorias", HttpMethod.POST, corpoRequisicao, String.class);
+
+        assertEquals(HttpStatus.BAD_REQUEST, corpoResposta.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Atualizar um Usuario")
+    public void deveAtualizarUmUsuario(){
+
+        Categoria categoriaCadastrada = categoriaRepository.save(new Categoria(0L, "Nova Categoria", "Descrição da nova categoria"));
+
+        Categoria categoriaUpdate = new Categoria(categoriaCadastrada.getId(), "Categoria Atualizada","A nova descrição da categoria");
+
+        HttpEntity<Categoria> corpoRequisicao = new HttpEntity<Categoria>(categoriaUpdate);
+
+        ResponseEntity<Categoria> corpoResposta = testRestTemplate
+                .exchange("/categorias", HttpMethod.PUT, corpoRequisicao, Categoria.class);
+    }
+
+    @Test
+    @DisplayName("Listar todos os Usuarios")
+    public void deveMostrarTodosUsuarios() {
+
+        categoriaRepository.save(new Categoria(0L, "Nova Categoria", "Descrição da nova categoria"));
+
+        categoriaRepository.save(new Categoria(0L, "Nova Categoria", "Descrição da nova categoria"));
+
+        ResponseEntity<String> resposta = testRestTemplate
+                .exchange("/categorias", HttpMethod.GET, null, String.class);
+
+        assertEquals(HttpStatus.OK, resposta.getStatusCode());
+    }
+}
