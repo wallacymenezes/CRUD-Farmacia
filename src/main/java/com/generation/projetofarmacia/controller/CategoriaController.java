724d95066e212059e2940020003bbe697bd2cf4d
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-07-22 08:51:37 -0300

    :heavy_plus_sign: Organizando o código

--- src/main/java/com/generation/projetofarmacia/controller/CategoriaController.java
+++ src/main/java/com/generation/projetofarmacia/controller/CategoriaController.java
@@ -1,5 +1,7 @@
 package com.generation.projetofarmacia.controller;
 
+import com.generation.projetofarmacia.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/categoria")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 }
--- src/main/java/com/generation/projetofarmacia/model/Categoria.java
+++ src/main/java/com/generation/projetofarmacia/model/Categoria.java
@@ -1,13 +1,46 @@
 package com.generation.projetofarmacia.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "O atributo não pode ser nulo e nem vazio.")
+    @Size(min = 1, max = 100, message = "O atributo deve ter no mínimo 5 e no máximo 100 caracteres")
+    private String nome;
+
+    @NotBlank(message = "O atributo não pode ser nulo e nem vazio.")
+    @Size(min = 5, max = 255, message = "O atributo deve ter no mínimo 5 e no máximo 255 caracteres")
+    private String descricao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
