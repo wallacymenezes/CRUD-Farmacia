1842d52dd664899d3c9f5fe5a5deeae3ce25a915
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-07-22 01:39:17 -0300

    Iniciando edições na branch

--- src/main/java/com/generation/projetofarmacia/model/Categoria.java
+++ src/main/java/com/generation/projetofarmacia/model/Categoria.java
@@ -1,9 +1,13 @@
 package com.generation.projetofarmacia.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_categoria")
 public class Categoria {
+
+    @Id
+    private long id;
 }
