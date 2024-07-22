246273cd1dc7a8a1683de573428d52c521c1fb4a
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-07-22 01:35:46 -0300

    :sparkles: feat: CrComeÃço da criação da estrutura para o CRUD de categoria

---
+++ src/main/java/com/generation/projetofarmacia/model/Categoria.java
@@ -0,0 +1,9 @@
+package com.generation.projetofarmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+}
---
+++ src/main/java/com/generation/projetofarmacia/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.generation.projetofarmacia.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository {
+}
