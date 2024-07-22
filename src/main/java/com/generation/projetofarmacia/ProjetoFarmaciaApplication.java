b608e4290e3e489f240a3a86e714882b5551de9e
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-07-22 01:28:50 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/generation/projetofarmacia/ProjetoFarmaciaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetofarmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFarmaciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFarmaciaApplication.class, args);
+    }
+
+}
